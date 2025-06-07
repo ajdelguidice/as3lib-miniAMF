0f5ae70783f09335a8553381d9bd1f4a0ef498da
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2025-06-07 11:40:24 -0500

    Remove the python 2 sets import from adapters._sets.py

--- miniamf/adapters/_sets.py
+++ miniamf/adapters/_sets.py
@@ -13,19 +13,5 @@ import miniamf
 def to_sorted_tuple(obj, encoder):
     return tuple(sorted(obj))
 
-
 miniamf.add_type(frozenset, to_sorted_tuple)
 miniamf.add_type(set, to_sorted_tuple)
-
-# The sets module was removed in Python 3.
-try:
-    ModuleNotFoundError
-except NameError:
-    ModuleNotFoundError = ImportError
-
-try:
-    import sets
-    miniamf.add_type(sets.ImmutableSet, to_sorted_tuple)
-    miniamf.add_type(sets.Set, to_sorted_tuple)
-except ModuleNotFoundError:
-    pass
