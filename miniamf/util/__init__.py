210c18dbcdfac15dd079dc1a42bdac7f05728a5d
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2025-06-11 09:40:09 -0500

    Make a slighly less wrong workarround for util.__init__.get_datetime
    This looks very jank but it seems to work like the original without using deprecated functions. I don't know if this works properly but datetime.datetime.fromtimestamp(-datetime.datetime.fromtimestamp(0).replace(tzinfo=datetime.timezone.utc).timestamp()) == datetime.datetime.utcfromti\
    mestamp(0) returns True so it shouldn't be that far off.

--- miniamf/util/__init__.py
+++ miniamf/util/__init__.py
@@ -62,7 +62,8 @@ def get_datetime(secs):
     # 1970-01-01T00:00:00Z through 2038-01-19T03:14:07Z.  Also, this way
     # fractional seconds are handled seamlessly (secs can be a float).
     #return (datetime.datetime.utcfromtimestamp(0) + datetime.timedelta(seconds=secs))
-    return (datetime.datetime.fromtimestamp(0,tz=datetime.timezone.utc) + datetime.timedelta(seconds=secs))
+    return datetime.datetime.fromtimestamp(-datetime.datetime.fromtimestamp(0).replace(tzinfo=datetime.timezone.utc).timestamp()) + datetime.timedelta(seconds=secs)
+
 
 
 def get_properties(obj):
