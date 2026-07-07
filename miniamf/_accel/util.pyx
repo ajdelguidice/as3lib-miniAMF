be6947cff8ca050aa591e34585dfa3075f397292
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-07-07 14:14:34 -0500

    Include PyFloat_Compat directly in _accel.util (#5)

--- miniamf/_accel/util.pyx
+++ miniamf/_accel/util.pyx
@@ -20,7 +20,25 @@ cdef extern from "Python.h":
     int PyByteArray_Check(object)
     char* PyByteArray_AsString(object)
 
-cdef extern from "PyFloat_Compat.h":
+cdef extern from *:
+    '''
+    #include <Python.h>
+
+    #if PY_VERSION_HEX < 0x030b0000
+    int PyFloat_Pack4(double x, char *p, int le) {
+        return _PyFloat_Pack4(x, (unsigned char *)p, le);
+    }
+    int PyFloat_Pack8(double x, char *p, int le) {
+        return _PyFloat_Pack8(x, (unsigned char *)p, le);
+    }
+    double PyFloat_Unpack4(const char *p, int le) {
+        return _PyFloat_Unpack4((const unsigned char*)p, le);
+    }
+    double PyFloat_Unpack8(const char *p, int le) {
+        return _PyFloat_Unpack8((const unsigned char*)p, le);
+    }
+    #endif
+    '''
     int PyFloat_Pack4(double, char *, int) except? -1
     int PyFloat_Pack8(double, char *, int) except? -1
     double PyFloat_Unpack4(const char *, int) except? -1.0
